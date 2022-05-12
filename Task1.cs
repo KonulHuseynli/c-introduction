80ba173ca373068422d8d576ab30148cb2bc2887
KonulHuseynli <konulrh@code.edu.az>
2022-05-12 11:49:28 +0400

    added new task

---
+++ Task1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = { 12, 7, 19, 36, 40, 58, 65, 87, 27, 23 };
+            Console.Write("Yasinizi daxil edin: ");
+            string yash = Console.ReadLine();
+            int Age = Int32.Parse(yash);
+            bool result = false;
+            foreach (int item in Array)
+            {
+                if (Age == item)
+                {
+                    result = true;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
+
